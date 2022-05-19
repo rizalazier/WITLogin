444f51f1b733b4d825e4a726ff95515f421fd186
Rizaldi Ilham Azib <rizalazier@gmail.com>
2022-05-19 13:22:08 +0700

    Add Users Dump

---
+++ SQL Dump/UsersInsert.sql
@@ -0,0 +1,19 @@
+USE [WebApiWit]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([id]
+           ,[Name]
+           ,[Username]
+           ,[Password])
+     VALUES
+           ('AFDEFA64-E72C-4382-A99D-857B5546293C'
+           ,'Varg Vikernes'
+           ,'vargvikernes'
+           ,'vik619'),
+
+		   ('ADB7E236-E805-4C9C-84C6-D6CF83645F91'
+           ,'WIT'
+           ,'witinaja'
+           ,'pass123')
+GO
